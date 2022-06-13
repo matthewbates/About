67dcc60b24aad1cba24f5b6a8a477167c17003f0
Matthew <matthewhcbates@gmail.com>
2022-06-12 20:24:21 -0600

    added TypeWriterEffect

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { HomeContainer, HomeImage, HomeH1 } from "./HomeElements";
 import Typewriter from "typewriter-effect";
+import TypeWriterEffect from "react-typewriter-effect";
 import Author from "../../assets/author.png";
 import Card from "../../components/Card";
 
@@ -10,18 +11,16 @@ export default function Home() {
       <Card>
         <h1>Hi, I'm Matthew</h1>
         <div className="subtitle-small subtitle">
-          <Typewriter
-            options={{
-              strings: [
-                "A passionate, creative technologist",
-                "Full-stack software developer",
-                "Freelance web designer",
-              ],
-              autoStart: true,
-              loop: true,
-              delay: 90,
-              deleteSpeed: 30,
-            }}
+          <TypeWriterEffect
+            multiText={[
+              "A passionate, creative technologist",
+              "Full-stack software developer",
+              "Freelance web designer",
+            ]}
+            multiTextDelay={2500}
+            textStyle={{ fontSize: "0.9rem", textAlign: "center" }}
+            cursorColor="#fff"
+            multiTextLoop
           />
         </div>
       </Card>
