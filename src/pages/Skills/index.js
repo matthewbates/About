869d23f3c0e2922b6e9074afdf12ae9bd417e46f
Matthew <matthewhcbates@gmail.com>
2022-08-08 11:41:58 -0600

    updated skills section; replaced resume.pdf

--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -18,23 +18,12 @@ export default function Skills() {
           <SkillsH1>Technologies</SkillsH1>
           {skillsIcons.map((item, index) => {
             return (
-              <>
-                <SkillsArticle>
-                  <SkillsImage src={item.img} alt="skills-icon" />
-                  <SkillsH2>{item.name}</SkillsH2>
-                </SkillsArticle>
-              </>
-            );
-          })}
-          {/* <SkillsH1>Currently Learning</SkillsH1>
-          {skillsLearning.map((item, index) => {
-            return (
-              <SkillsArticle>
+              <SkillsArticle key={index}>
                 <SkillsImage src={item.img} alt="skills-icon" />
                 <SkillsH2>{item.name}</SkillsH2>
               </SkillsArticle>
             );
-          })} */}
+          })}
         </SkillsDiv>
       </Card>
     </SkillsContainer>
