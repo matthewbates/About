7001729fa04fab0c333e3c2aeb7530143e10b5a1
Matthew <matthewhcbates@gmail.com>
2022-10-11 13:48:32 -0600

    updated RightNav

--- src/components/NavLinks/NavLinkElements.js
+++ src/components/NavLinks/NavLinkElements.js
@@ -14,6 +14,10 @@ export const LinksContainer = styled.li`
   padding: 0 0.25rem;
   transition: color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
   font-family: "Lato";
+
+  @media screen and (max-width: 768px) {
+    margin-top: 1rem;
+  }
 `;
 
 export const LinkScroll = styled(Link)`
--- src/components/RightNav/RightNavElements.js
+++ src/components/RightNav/RightNavElements.js
@@ -4,7 +4,7 @@ import CONSTANTS from "../../utils/constants";
 export const UlStyle = styled.aside`
   position: fixed;
   z-index: 999;
-  width: 45%;
+  width: 40%;
   height: 100%;
   padding-top: 3.5rem;
   flex-flow: column nowrap;
@@ -13,6 +13,10 @@ export const UlStyle = styled.aside`
   background: ${CONSTANTS.colors.batman};
   right: ${({ open }) => (open ? "0" : "-100%")};
   opacity: ${({ open }) => (open ? 1 : 0)};
+
+  @media screen and (max-width: 768px) {
+    width: 50%;
+  }
 `;
 
 export const AuthorImg = styled.img`
@@ -24,38 +28,9 @@ export const AuthorImg = styled.img`
 
 export const AuthorName = styled.h1`
   color: ${CONSTANTS.colors.persephoneWhite};
-  margin-top: 1rem;
+  margin-top: 2rem;
   font-size: 25px;
   display: block;
   text-align: center;
   font-family: "Lato";
-`;
-
-export const RightNavBtn = styled.div`
-  align-items: center;
-  text-align: center;
-  justify-content: center;
-`;
-
-export const RightNavButton = styled.button`
-  text-align: center;
-  justify-content: center;
-  align-items: center;
-  border-radius: 6px;
-  margin-top: 2rem;
-  white-space: nowrap;
-  padding: ${({ big }) => (big ? "14px 48px" : "12px 30px")};
-  font-size: ${({ fontBig }) => (fontBig ? "20px" : " 16px")};
-  outline: none;
-  border: none;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  font-family: "Lato";
-
-  &:hover {
-    transition: all 0.2s ease-in-out;
-    filter: brightness(80%);
-    color: ${({ primary }) =>
-      primary ? `${CONSTANTS.colors.eggshellWhite}` : "black"};
-  }
 `;
